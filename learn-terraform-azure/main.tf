eb42cb0b22be4c5b0ea545e80323a6645e3d68a9
BishnuKumar Bhakat <bishnukumar_bhakat@epam.com>
2024-03-22 13:15:29 +0530

    changes

---
+++ learn-terraform-azure/main.tf
@@ -0,0 +1,34 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.92.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+
+resource "azurerm_resource_group" "front_end_rg" {
+  name     = "myTFResourceGroup"
+  location = "southeastasia"
+}
+
+resource "azurerm_storage_account" "front_end_storage_account" {
+  name                     = "bkbstgsandfrontendne001"
+  resource_group_name      = azurerm_resource_group.front_end_rg.name
+  location                 = "southeastasia"
+  account_kind             = "StorageV2"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+
+  static_website {
+    index_document = "index.html"
+  }
+}
