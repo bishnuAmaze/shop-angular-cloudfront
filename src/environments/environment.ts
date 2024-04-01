c1267be2d26bd7c67513b740c551c0f59e17e741
BishnuKumar Bhakat <bishnukumar_bhakat@epam.com>
2024-04-01 18:49:01 +0530

    rectification

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment: Config = {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    bff: 'https://xrnb4sknsh.execute-api.eu-west-1.amazonaws.com/dev',
+    bff: 'https://7tlw24mwy0.execute-api.eu-west-1.amazonaws.com/dev',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
   apiEndpointsEnabled: {
